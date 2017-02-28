43d2c5b3df057bcafc39ed3cb5f8bcdb773e7c0d
Stoeffel <schtoeffel@gmail.com>
2017-02-28 14:14:06 +0100

    move tasks

--- src/Lib.hs
+++ src/Lib.hs
@@ -6,3 +6,12 @@ import qualified Config
 
 run :: IO ()
 run = putStrLn "run"
+  -- 1. read config
+  -- 2. find dependencies of entry points (magic requires (.sass/.elm)?) (how many levels? we should probably find elm deps as wel)
+  -- 3. create binary with tree
+  -- 3. build cache (not mvp)
+  --    a. create binary with file modified date and hash of content
+  -- 4. compile
+  --    a. check cache if we need to build this. first date and the hash
+  -- 5. recurisvely replace requires
+  -- 6. compress (if prod)
