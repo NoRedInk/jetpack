b985cbbadc01ac4826368be9174399687ed11232
Stoeffel <schtoeffel@gmail.com>
2017-03-01 15:11:06 +0100

    use filepath instead of text

--- src/Require.hs
+++ src/Require.hs
@@ -10,11 +10,12 @@ module Require
 
 import qualified Data.Text as T
 import Errors (Error(..))
+import System.FilePath ((<.>), splitExtension)
 import qualified Text.Parsec as P
 
 data Require = Require
-  { fileName :: T.Text
-  , fileType :: SourceType
+  { fileType :: SourceType
+  , fileName :: FilePath
   } deriving (Show, Eq)
 
 data SourceType
@@ -32,16 +33,16 @@ require content =
   case extractRequire content of
     Right (name, ext) ->
       case getFileType ext of
-        Unsupported -> Right $ Require (T.concat [name, ext]) Js
-        _ -> Right $ Require (T.dropEnd 1 name) $ getFileType ext
+        Unsupported -> Right $ Require Js $ name <.> ext
+        _ -> Right $ Require (getFileType ext) name
     Left msg -> Left $ T.pack $ show msg
 
 {-| running the parser
 -}
-extractRequire :: T.Text -> Either P.ParseError (T.Text, T.Text)
+extractRequire :: T.Text -> Either P.ParseError (FilePath, String)
 extractRequire str = P.parse requireParser "Error" str
 
-requireParser :: P.Parsec T.Text u0 (T.Text, T.Text)
+requireParser :: P.Parsec T.Text u0 (FilePath, String)
 requireParser = do
   _ <- P.manyTill P.anyChar (P.lookAhead $ P.try requireKeyword)
   _ <- requireKeyword
@@ -49,13 +50,13 @@ requireParser = do
   _ <- P.oneOf "'\""
   content <- P.manyTill P.anyChar (P.oneOf "'\"")
   _ <- P.oneOf " )\n"
-  return $ T.breakOnEnd "." $ T.pack content
+  return $ splitExtension content
   where
     requireKeyword = P.string "require"
 
-getFileType :: T.Text -> SourceType
-getFileType "coffee" = Coffee
-getFileType "elm" = Elm
-getFileType "sass" = Sass
-getFileType "js" = Js
+getFileType :: String -> SourceType
+getFileType ".coffee" = Coffee
+getFileType ".elm" = Elm
+getFileType ".sass" = Sass
+getFileType ".js" = Js
 getFileType _ = Unsupported
--- test/RequireSpec.hs
+++ test/RequireSpec.hs
@@ -6,6 +6,7 @@ import Control.Monad.Trans.Either (runEitherT)
 import Data.Text as T
 import qualified Require
 import qualified Test.SmallCheck.Series.Text as T.Series
+import System.FilePath ((<.>))
 import Test.Tasty
 import Test.Tasty.HUnit
 import Test.Tasty.SmallCheck
@@ -21,29 +22,29 @@ suite =
   testGroup
     "Require"
     [ testCase "#require" $
-      assertRequire "var x = require(\"x.elm\")" $ Require.Require "x" Require.Elm
+      assertRequire "var x = require(\"x.elm\")" $ Require.Require Require.Elm "x"
     , testCase "#require" $
-      assertRequire "let foo = require(\"foo.elm\")" $ Require.Require "foo" Require.Elm
+      assertRequire "let foo = require(\"foo.elm\")" $ Require.Require Require.Elm "foo"
     , testCase "#require" $
-      assertRequire "require(\"foo.bar.elm\")" $ Require.Require "foo.bar" Require.Elm
+      assertRequire "require(\"foo.bar.elm\")" $ Require.Require Require.Elm $ "foo" <.> "bar"
     , testCase "#require" $
-      assertRequire "require(\"foo.bar.coffee\")" $ Require.Require "foo.bar" Require.Coffee
+      assertRequire "require(\"foo.bar.coffee\")" $ Require.Require Require.Coffee $ "foo" <.> "bar"
     , testCase "#require" $
-      assertRequire "require(\"foo.bar.sass\")" $ Require.Require "foo.bar" Require.Sass
+      assertRequire "require(\"foo.bar.sass\")" $ Require.Require Require.Sass $ "foo" <.> "bar"
     , testCase "#require" $
-      assertRequire "require(\"foo.bar\")" $ Require.Require "foo.bar" Require.Js
+      assertRequire "require(\"foo.bar\")" $ Require.Require Require.Js $ "foo" <.> "bar"
     , testCase "#require" $
-      assertRequire "require(\"foo.bar.js\")" $ Require.Require "foo.bar" Require.Js
+      assertRequire "require(\"foo.bar.js\")" $ Require.Require Require.Js $ "foo" <.> "bar"
     , testCase "#require" $
-      assertRequire "coffee = require \"foo.bar.js\" " $ Require.Require "foo.bar" Require.Js
+      assertRequire "coffee = require \"foo.bar.js\" " $ Require.Require Require.Js $ "foo" <.> "bar"
     , testCase "#require" $
-      assertRequire "require 'foo.bar.js' " $ Require.Require "foo.bar" Require.Js
+      assertRequire "require 'foo.bar.js' " $ Require.Require Require.Js $ "foo" <.> "bar"
     , testCase "#require multilines" $
       assertRequire (T.unlines ["// test", "moo = require 'foo.bar.js'", "moo(42)"]) $
-      Require.Require "foo.bar" Require.Js
+        Require.Require Require.Js $ "foo" <.> "bar"
     , testCase "#require multilines" $
       assertRequire (T.unlines ["// test", "var moo = require('foo.bar.js');", "moo(42)"]) $
-      Require.Require "foo.bar" Require.Js
+        Require.Require Require.Js $ "foo" <.> "bar"
     ]
 
 properties :: TestTree
@@ -52,6 +53,6 @@ properties =
     "Require Properties"
     [ testProperty "#require" $ \name ->
         case Require.require (T.concat ["require '", T.pack name, ".js' "]) of
-          Right c -> c == Require.Require (T.pack name) Require.Js
+          Right c -> c == Require.Require Require.Js name
           Left msg -> False
     ]
