f4ce04da9ba0f0471d7874aa846edcceaea265fc
Stoeffel <schtoeffel@gmail.com>
2017-03-07 09:35:28 +0100

    basic free

--- app/Main.hs
+++ app/Main.hs
@@ -1,9 +1,60 @@
+{-# OPTIONS_GHC -Wall #-}
+{-# LANGUAGE DeriveFunctor #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
+
 module Main where
 
+import Control.Monad.Free (Free, foldFree, liftF)
+import Data.String (IsString)
+import Data.Text as T
 import Lib (run)
 
+newtype Args =
+  Args T.Text
+  deriving (IsString)
+
+newtype Config =
+  Config T.Text
+  deriving (IsString)
+
+newtype Module =
+  Module T.Text
+  deriving (IsString)
+
+data PipelineF a
+  = ReadCliArgs (Args -> a)
+  | ReadConfig Args
+               (Config -> a)
+  | Compile ([Module] -> a)
+  deriving (Functor)
+
+type Pipeline = Free PipelineF
+
+readCliArgs :: Pipeline Args
+readCliArgs = liftF $ ReadCliArgs id
+
+readConfig :: Args -> Pipeline Config
+readConfig path = liftF $ ReadConfig path id
+
+compile :: Pipeline [Module]
+compile = liftF $ Compile id
+
+program :: Pipeline ()
+program = do
+  args <- readCliArgs
+  config <- readConfig args
+  modules <- compile
+  return ()
+
+stepPrintI :: PipelineF a -> IO a
+stepPrintI (ReadCliArgs next) = putStrLn "readCliArgs" >> return (next "")
+stepPrintI (ReadConfig args next) = putStrLn "readConfig" >> return (next "")
+stepPrintI (Compile next) = putStrLn "compile" >> return (next [])
+
 main :: IO ()
-main = run
+main = do
+  foldFree stepPrintI program
   -- 1. read config
   -- 2. find dependencies of entry points (magic requires (.sass/.elm)?) (how many levels? we should probably find elm deps as wel)
   -- 3. create binary with tree
