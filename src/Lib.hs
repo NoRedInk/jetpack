7c56363356ed9b4898b94cec3b972b95a434a72e
Stoeffel <schtoeffel@gmail.com>
2017-03-08 09:43:35 +0100

    remove warnings

--- src/Lib.hs
+++ src/Lib.hs
@@ -6,12 +6,11 @@ module Lib
   ( run
   ) where
 
-import qualified Config
-import Control.Monad.Free (Free, foldFree, liftF, hoistFree)
+import Config ()
+import Control.Monad.Free (Free, foldFree)
 import Control.Monad.Trans.Class
 import Control.Monad.Trans.Either
 import Data.Functor.Sum (Sum(..))
-import Data.Text as T
 import qualified Error
 import qualified Interpreter.Logger as LogI
 import qualified Interpreter.Pipeline as PipelineI
@@ -21,27 +20,29 @@ import qualified System.Exit
 import Task
 import Utils.Free (toLeft, toRight)
 
+run :: IO ()
+run = do
+  e <- runEitherT $ do runProgram program
+  case e of
+    Left err -> do
+      putStrLn "Compilation failed!"
+      System.Exit.die $ Error.description err
+    Right _ -> putStrLn "Compilation succeeded!"
+
 program :: Pipeline ()
 program = do
-  args <- readCliArgs
-  config <- readConfig (configPath args)
+  args <- readCliArgs -- TODO we propably want to read cli args before running the program.
+  _config <- readConfig (configPath args)
   _ <- compile
   return ()
+
+runProgram :: Pipeline a -> Task a
+runProgram = foldFree executor . foldFree interpreter
 
 interpreter :: PipelineF a -> Free (Sum Logger.LogF Task) a
 interpreter op =
   toLeft (LogI.interpreter op) *> toRight (lift $ PipelineI.interpreter op)
 
 executor :: Sum Logger.LogF Task a -> Task a
-executor (InL log@(Logger.Log _ _ next)) =
-  lift $ Logger.executor log >> return next
+executor (InL l@(Logger.Log _ _ next)) = lift $ Logger.executor l >> return next
 executor (InR io) = io
-
-run :: IO ()
-run = do
-  e <- runEitherT $ do foldFree executor $ foldFree interpreter program
-  case e of
-    Left err -> do
-      putStrLn "Compilation failed!"
-      System.Exit.die $ Error.description err
-    Right _ -> putStrLn "Compilation succeeded!"
