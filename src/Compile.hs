0895cd166c37cac51a9da62512a8374dfe792a1a
Michael Glass <me@mike.is>
2017-03-14 11:50:33 +0100

    - Thread config through to elmCompiler & outputFileName - Use runCmd for coffee

--- src/Compile.hs
+++ src/Compile.hs
@@ -1,22 +1,20 @@
 {-# OPTIONS_GHC -Wall #-}
 {-# LANGUAGE DeriveFunctor     #-}
+{-# LANGUAGE NamedFieldPuns    #-}
 {-# LANGUAGE OverloadedStrings #-}
 
 {-|
 -}
 module Compile where
 
-import Config (Config)
+import Config (Config (..))
 import Control.Concurrent.Async.Lifted as Async
-import Control.Monad.Except (throwError)
 import Control.Monad.Trans.Class (lift)
 import Data.List as L
 import Data.Text as T
 import Dependencies (Dependency (..))
-import Error (Error (..))
 import GHC.IO.Handle
 import Parser.Ast as Ast
-import System.Exit
 import System.FilePath ((<.>), (</>))
 import System.Process
 import Task (Task)
@@ -25,47 +23,41 @@ import Utils.Files (pathToFileName)
 newtype Compiler = Compiler { runCompiler :: FilePath -> FilePath -> Task () }
 
 compileModules :: Config -> [Dependency] -> Task ()
-compileModules _ modules = Async.forConcurrently_ modules compile
+compileModules config modules = Async.forConcurrently_ modules $ compile config
 
 {-| Compile a dependency.
  1. find compiler
  2. create output path
  3. compile to that output path
 -}
-compile :: Dependency -> Task ()
-compile (Dependency Ast.Elm _ p)    = (runCompiler elmCompiler) p $ outputFileName p
-compile (Dependency Ast.Js _ p)     = (runCompiler jsCompiler) p "test"
-compile (Dependency Ast.Coffee _ p) = (runCompiler coffeeCompiler) p $ "ui" </> outputFileName p
-compile (Dependency Ast.Sass _ p)   = (runCompiler sassCompiler) p $ sassOutputFileName p
+compile :: Config -> Dependency -> Task ()
+compile config (Dependency Ast.Elm _ p)    = (runCompiler $ elmCompiler config) p $ buildArtifactPath config "js" p
+compile _config (Dependency Ast.Js _ p)     = (runCompiler jsCompiler ) p "test"
+compile config (Dependency Ast.Coffee _ p) = (runCompiler coffeeCompiler ) p $ buildArtifactPath config "js" p -- todo get rid of ui here
+compile config (Dependency Ast.Sass _ p)   = (runCompiler sassCompiler ) p $ buildArtifactPath config "css" p
 
 
-outputFileName :: FilePath -> String
-outputFileName path =
-  "."
-  </> "tmp" -- TODO use path from config
-  </> (T.unpack $ pathToFileName path)
-  <.> "js"
-
-sassOutputFileName :: FilePath -> String
-sassOutputFileName path =
-  "."
-  </> "ui" -- TODO use path from config
-  </> "tmp" -- TODO use path from config
-  </> (T.unpack $ pathToFileName path)
-  <.> "css"
+buildArtifactPath :: Config -> String -> FilePath -> String
+buildArtifactPath Config{temp_directory} extension inputPath=
+  temp_directory
+  </> (T.unpack $ pathToFileName inputPath)
+  <.> extension
 
 ---------------
 -- COMPILERS --
 ---------------
 
-elmCompiler :: Compiler
-elmCompiler = Compiler $ \input output -> do
-  let elmMake = "elm-make " ++ "../" ++ input ++ " --output " ++ output
-  runCmd elmMake "./ui" -- TODO use path from config
+elmCompiler :: Config -> Compiler
+elmCompiler Config{elm_root_directory} = Compiler $ \input output -> do
+  -- TODO use elm_root_directory instead of the "../" below
+  -- also pass in absolute paths
+  let elmMake = "elm-make " ++ "../" ++ input ++ " --output " ++ "../" ++ output
+  runCmd elmMake $ Just elm_root_directory
 
 coffeeCompiler :: Compiler
 coffeeCompiler = Compiler $ \input output -> do
-  runCoffee input output
+  let coffee = "coffee -p " ++ input ++ " > " ++ output
+  runCmd coffee Nothing
 
 {-| The js compiler will basically only copy the file into the tmp dir.
 -}
@@ -78,7 +70,7 @@ jsCompiler = Compiler $ \_input _output -> do
 sassCompiler :: Compiler
 sassCompiler = Compiler $ \input output -> do
   let sassc = "sassc " ++ input ++ " " ++ output ++ " --load-path " ++ loadPath -- <= include stuff that is needed in load-path
-  runCmd sassc "./" -- TODO use path from config
+  runCmd sassc Nothing
   where
     loadPath = L.intercalate (":")
       -- TODO move this to config
@@ -91,27 +83,20 @@ sassCompiler = Compiler $ \input output -> do
       , "node_modules" </> "bourbon-neat" </> "app" </> "assets" </> "stylesheets"
       ]
 
-runCmd :: String -> String -> Task ()
-runCmd cmd cwd = do
+runCmd :: String -> Maybe String -> Task ()
+runCmd cmd maybeCwd = do
+  -- TODO: handle exit status here
   (_, maybeOut, _, _) <- lift $ createProcess (proc "bash" ["-c", cmd])
     { std_out = CreatePipe
-    , cwd = Just cwd
+    , cwd = maybeCwd
     }
   printStdOut maybeOut
   return ()
 
-runCoffee :: String -> String -> Task ()
-runCoffee input output = do
-  let cmd = "coffee -p " ++ input ++ " > " ++ output
-  (e,_,_) <- lift $ readProcessWithExitCode "bash" ["-c", cmd] ""
-  if e == ExitSuccess then
-    return ()
-  else
-    throwError [JsonInvalid input]
 
 printStdOut :: Maybe Handle -> Task ()
 printStdOut (Just out) = lift $ do
                   contents <- hGetContents out
-                  -- putStrLn contents
+                  putStrLn contents
                   return ()
 printStdOut Nothing = lift $ do return ()
