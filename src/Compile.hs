0f511e0b7c3b473434b57d9b59cf7ad448c4c9ee
Stoeffel <schtoeffel@gmail.com>
2017-04-06 12:12:22 +0200

    remove progress bar

--- src/Compile.hs
+++ src/Compile.hs
@@ -8,7 +8,7 @@ module Compile where
 
 import Config (Config (..))
 -- import Control.Concurrent.Async.Lifted as Async (forConcurrently)
-import Control.Monad.Except (runExceptT, throwError)
+import Control.Monad.Except (throwError)
 import Control.Monad.Trans.Class (lift)
 import Data.List as L
 import Data.Text as T
@@ -16,7 +16,6 @@ import Data.Time.Clock
 import Dependencies (Dependency (..))
 import GHC.IO.Handle
 import Parser.Ast as Ast
-import System.Console.AsciiProgress
 import System.Directory (copyFile)
 import System.Exit
 import System.FilePath ((</>))
@@ -29,38 +28,24 @@ newtype Compiler = Compiler { runCompiler :: FilePath -> FilePath -> Task [T.Tex
 
 compileModules :: Config -> ToolPaths ->  [Dependency] -> Task ()
 compileModules config@Config {log_directory} toolPaths modules = do
-  e <- lift $ displayConsoleRegions $ do
-    pg <- newProgressBar def
-      { pgTotal = toInteger $ L.length modules
-      , pgOnCompletion = Just "Compiled :percent after :elapsed seconds"
-      , pgCompletedChar = '█'
-      , pgPendingChar = '░'
-      , pgFormat = "Compiling ╢:bar╟ :current/:total"
-      }
-    e <- runExceptT
-      $ traverse (compile pg config toolPaths) modules
-    complete pg
-    return e
-  case e of
-    Left err -> throwError err
-    Right output  -> do
-      let log = T.unlines $ fmap T.unlines output
-      lift $ writeFile (log_directory </> "compile.log") $ T.unpack log
-      return ()
+  output <- traverse (compile config toolPaths) modules
+  let log = T.unlines $ fmap T.unlines output
+  lift $ writeFile (log_directory </> "compile.log") $ T.unpack log
+  return ()
 
-compile :: ProgressBar -> Config -> ToolPaths ->  Dependency -> Task [T.Text]
-compile pg config toolPaths Dependency {fileType, filePath} = do
-  let (c, outputType) = compiler fileType config toolPaths pg
+compile :: Config -> ToolPaths ->  Dependency -> Task [T.Text]
+compile config toolPaths Dependency {fileType, filePath} = do
+  let (c, outputType) = compiler fileType config toolPaths
   let outputPath = buildArtifactPath config outputType filePath
   (runCompiler c) filePath outputPath
 
-compiler :: Ast.SourceType -> Config -> ToolPaths -> ProgressBar -> (Compiler, String)
-compiler fileType config ToolPaths{elmMake, sassc, coffee} pg =
+compiler :: Ast.SourceType -> Config -> ToolPaths -> (Compiler, String)
+compiler fileType config ToolPaths{elmMake, sassc, coffee} =
   case fileType of
-    Ast.Elm    -> (elmCompiler pg config elmMake, "js")
-    Ast.Js     -> (jsCompiler pg, "js")
-    Ast.Coffee -> (coffeeCompiler pg coffee, "js")
-    Ast.Sass   -> (sassCompiler pg config sassc, "css")
+    Ast.Elm    -> (elmCompiler config elmMake, "js")
+    Ast.Js     -> (jsCompiler, "js")
+    Ast.Coffee -> (coffeeCompiler coffee, "js")
+    Ast.Sass   -> (sassCompiler config sassc, "css")
 
 buildArtifactPath :: Config -> String -> FilePath -> String
 buildArtifactPath Config{temp_directory} extension inputPath =
@@ -70,36 +55,35 @@ buildArtifactPath Config{temp_directory} extension inputPath =
 -- COMPILERS --
 ---------------
 
-elmCompiler :: ProgressBar -> Config -> FilePath -> Compiler
-elmCompiler pg Config{elm_root_directory} elmMake = Compiler $ \input output -> do
+elmCompiler :: Config -> FilePath -> Compiler
+elmCompiler Config{elm_root_directory} elmMake = Compiler $ \input output -> do
   -- TODO use elm_root_directory instead of the "../" below
   -- also pass in absolute paths
   let cmd = elmMake ++ " " ++ "../" ++ input ++ " --output " ++ "../" ++ output
-  runCmd pg cmd $ Just elm_root_directory
+  runCmd cmd $ Just elm_root_directory
 
-coffeeCompiler :: ProgressBar -> FilePath -> Compiler
-coffeeCompiler pg coffee = Compiler $ \input output -> do
+coffeeCompiler :: FilePath -> Compiler
+coffeeCompiler coffee = Compiler $ \input output -> do
   let cmd = coffee ++ " -p " ++ input ++ " > " ++ output
-  runCmd pg cmd Nothing
+  runCmd cmd Nothing
 
 {-| The js compiler will basically only copy the file into the tmp dir.
 -}
-jsCompiler :: ProgressBar -> Compiler
-jsCompiler pg = Compiler $ \input output -> lift $ do
+jsCompiler :: Compiler
+jsCompiler = Compiler $ \input output -> lift $ do
   copyFile input output
-  tick pg
   currentTime <- getCurrentTime
   let commandFinished = T.pack $ show currentTime
   return [commandFinished, T.concat ["moved ", T.pack input, " => ", T.pack output]]
 
-sassCompiler :: ProgressBar -> Config -> FilePath -> Compiler
-sassCompiler pg Config {sass_load_paths} sassc = Compiler $ \input output -> do
+sassCompiler :: Config -> FilePath -> Compiler
+sassCompiler Config {sass_load_paths} sassc = Compiler $ \input output -> do
   let loadPath = L.intercalate ":" sass_load_paths
   let cmd = "SASS_PATH=" ++ loadPath ++ " " ++ sassc ++ " " ++ input ++ " " ++ output
-  runCmd pg cmd Nothing
+  runCmd cmd Nothing
 
-runCmd :: ProgressBar -> String -> Maybe String -> Task [T.Text]
-runCmd pg cmd maybeCwd = do
+runCmd :: String -> Maybe String -> Task [T.Text]
+runCmd cmd maybeCwd = do
   -- TODO: handle exit status here
   (_, Just out, _, ph) <- lift $ createProcess (proc "bash" ["-c", cmd])
     { std_out = CreatePipe
@@ -109,7 +93,6 @@ runCmd pg cmd maybeCwd = do
   case ec of
       ExitSuccess   -> lift $ do
         content <- hGetContents out
-        tick pg
         currentTime <- getCurrentTime
         let commandFinished = T.pack $ show currentTime
         return [commandFinished, T.pack cmd, T.pack content]
