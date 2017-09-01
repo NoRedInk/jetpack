d129b3eef17aa32a7682e4e9961c2892abd28c87
Stoeffel <schtoeffel@gmail.com>
2017-09-01 22:28:59 +0200

    fix release.sh

--- release.sh
+++ release.sh
@@ -6,13 +6,13 @@ else
   ./package/mac/build-package.sh
   ./package/linux/build-in-docker.sh
 
+  sed -i '' "s/^version:\s*.*/version: ${1}/g" jetpack.cabal
+  git add jetpack.cabal
+
   git add ./binaries
   git commit -m "update binaries"
   git push
 
   git tag -a $1 -m "$1"
   git push --tags
-
-  # updates version in src/Version.hs
-  sed -i '' "s/^version:\s*.*/version: ${1}/g" jetpack.cabal
 fi
