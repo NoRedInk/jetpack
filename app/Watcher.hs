5a968490c03791850ee120047ddb5795d49ede86
Christoph Hermann <schtoeffel@gmail.com>
2017-12-07 10:28:33 +0100

    Watch in jetpack (#23)
    * Hello world watching files using Twitch
    
    * Moved watcher to a separate executable
    
    * Watcher running in separate executable.
    
    Also, debounce time was reduced and the main compilation process is now spawned
    on a different thread.
    
    * Stop current compilation and rebuild if a change occurs
    
    * Tidy up
    
    * no logger
    
    * short delay before running
    
    * formatting
    
    * watching for all types and take source dir from config
    
    * longer delay
    
    * cleanup killing thread :skull:
    
    * moving twitch to watchers dependencies
    
    * remove unused code
    
    * more cleanup
    
    * build initially
    
    * add note about racecondition

---
+++ app/Watcher.hs
@@ -0,0 +1,83 @@
+{-# LANGUAGE OverloadedStrings #-}
+
+module Main where
+
+import qualified Config
+import Control.Concurrent
+import Control.Monad.Except (throwError)
+import Data.Foldable (for_)
+import qualified Data.Text as T
+import Error
+import GHC.IO.Handle
+import qualified System.Console.AsciiProgress as Progress
+import qualified System.Directory as Dir
+import System.Environment
+import System.Exit
+import System.FilePath ()
+import System.Process
+import Task (Task, toTask)
+import qualified Task
+import Twitch
+    ( DebounceType (..)
+    , Dep
+    , LoggerType (..)
+    , Options (..)
+    , addModify
+    , defaultMainWithOptions
+    )
+
+main :: IO ()
+main = Progress.displayConsoleRegions $ do
+  cwd <- Dir.getCurrentDirectory
+  maybeConfig <- Task.runTask $ Config.load cwd
+  case maybeConfig of
+    Right (Just config) -> watch config
+    _                   -> putStrLn "no jetpack config found."
+
+watch :: Config.Config -> IO ()
+watch config = do
+  mVar <- newEmptyMVar
+  rebuild mVar
+  defaultMainWithOptions (options config)
+    $ for_ fileTypesToWatch
+    $ addModify
+    $ const
+    $ rebuild mVar
+
+fileTypesToWatch :: [Dep]
+fileTypesToWatch =
+  [ "**/*.elm"
+  , "**/*.coffee"
+  , "**/*.js"
+  , "**/*.sass"
+  , "**/*.scss"
+  , "**/*.json"
+  ]
+
+options :: Config.Config -> Options
+options config =
+  Options
+    NoLogger -- log
+    Nothing -- logFile
+    (Just $ Config.source_directory config) -- root
+    True -- recurseThroughDirectories
+    Twitch.Debounce -- debounce
+    (10^6) -- debounceAmount (1sec)
+    0 -- pollInterval
+    False -- usePolling
+
+rebuild :: MVar ProcessHandle -> IO ()
+rebuild mVar = do
+  runningProcess  <- tryTakeMVar mVar
+  -- NOTE: there might be a race condition here,
+  -- where we didn't add the process handle to the MVar yet.
+  for_ runningProcess terminateProcess
+  ph <- run
+  putMVar mVar ph
+
+run :: IO ProcessHandle
+run = do
+  args <- getArgs
+  let argsAsString = unwords args
+  (_, _, _, ph) <- createProcess (proc "bash" ["-c", "jetpack " ++ argsAsString]) { cwd = Nothing }
+  return ph
