c9e2ec7b76c61fae4a0e962bd8eb3cf423ecda52
Juan Edi <juan@noredink.com>
2017-12-08 13:07:14 -0300

    jetpack-watch: fix race condition when writing multiple files at once.

--- app/Watcher.hs
+++ app/Watcher.hs
@@ -33,7 +33,7 @@ main = do
 
 watch :: Config.Config -> IO ()
 watch config = do
-  mVar <- newEmptyMVar
+  mVar <- newMVar Nothing
   putStrLn "Watching. Hit ctrl-c to exit."
   rebuild mVar
   defaultMainWithOptions (options config) $
@@ -55,15 +55,16 @@ options config =
     0 -- pollInterval
     False -- usePolling
 
-rebuild :: MVar ProcessID -> IO ()
+rebuild :: MVar (Maybe ProcessID) -> IO ()
 rebuild mVar = do
-  runningProcess <- tryTakeMVar mVar
-  -- NOTE: there might be a race condition here,
-  -- where we didn't add the process handle to the MVar yet.
+  -- takeMVar blocks if there is nothing inside it.
+  -- This prevents a race condition that could happen if multiple files are
+  -- written at once.
+  runningProcess <- takeMVar mVar
   for_ runningProcess (signalProcess softwareTermination)
   for_ runningProcess (getProcessStatus True False) -- here be dragons, potentially
   processID <- run
-  putMVar mVar processID
+  putMVar mVar (Just processID)
 
 run :: IO ProcessID
 run = do
