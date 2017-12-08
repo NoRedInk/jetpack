78cb1d8ee39df0ccc960ab0da54c816e92038d44
Brian Hicks <brian@brianthicks.com>
2017-12-08 03:47:53 -0600

    Watch pairing (#27)
    * disable debounce
    
    It doesn't work with files changed multiple times in Emacs, and leaves
    the system in an inconsistent state.
    
    * remove displayConsoleRegion
    
    * use default debounce
    
    see previous commit about Emacs. This works better!
    
    * fork Haskelly jetpack without requiring separate binary
    
    * remove the "type to exit" line from twitch
    
    * use ridiculous vendored code solution

--- app/Watcher.hs
+++ app/Watcher.hs
@@ -7,30 +7,34 @@ import Data.Foldable (for_)
 import qualified Data.Text as T
 import Error
 import GHC.IO.Handle
-import qualified System.Console.AsciiProgress as Progress
+import qualified Lib
 import qualified System.Directory as Dir
 import System.Environment
 import System.Exit
+import qualified System.FSNotify as FS
 import System.FilePath ()
+import System.Posix.Process
+import System.Posix.Signals
+import System.Posix.Types (ProcessID)
 import System.Process
 import Task (Task, toTask)
 import qualified Task
 import Twitch
-       (DebounceType(..), Dep, LoggerType(..), Options(..), addModify,
-        defaultMainWithOptions)
+       (DebounceType(..), Dep, LoggerType(..), Options(..), addModify)
+import Twitch.Extra (defaultMainWithOptions)
 
 main :: IO ()
-main =
-  Progress.displayConsoleRegions $ do
-    cwd <- Dir.getCurrentDirectory
-    maybeConfig <- Task.runTask $ Config.load cwd
-    case maybeConfig of
-      Right (Just config) -> watch config
-      _ -> putStrLn "no jetpack config found."
+main = do
+  cwd <- Dir.getCurrentDirectory
+  maybeConfig <- Task.runTask $ Config.load cwd
+  case maybeConfig of
+    Right (Just config) -> watch config
+    _ -> putStrLn "no jetpack config found."
 
 watch :: Config.Config -> IO ()
 watch config = do
   mVar <- newEmptyMVar
+  putStrLn "Watching. Hit ctrl-c to exit."
   rebuild mVar
   defaultMainWithOptions (options config) $
     for_ fileTypesToWatch $ addModify $ const $ rebuild mVar
@@ -46,25 +50,24 @@ options config =
     Nothing -- logFile
     (Just $ Config.source_directory config) -- root
     True -- recurseThroughDirectories
-    Twitch.Debounce -- debounce
-    (10 ^ 6) -- debounceAmount (1sec)
+    Twitch.DebounceDefault -- debounce
+    0 -- debounce interval (required but not used)
     0 -- pollInterval
     False -- usePolling
 
-rebuild :: MVar ProcessHandle -> IO ()
+rebuild :: MVar ProcessID -> IO ()
 rebuild mVar = do
   runningProcess <- tryTakeMVar mVar
   -- NOTE: there might be a race condition here,
   -- where we didn't add the process handle to the MVar yet.
-  for_ runningProcess terminateProcess
-  ph <- run
-  putMVar mVar ph
+  for_ runningProcess (signalProcess softwareTermination)
+  for_ runningProcess (getProcessStatus True False) -- here be dragons, potentially
+  processID <- run
+  putMVar mVar processID
 
-run :: IO ProcessHandle
+run :: IO ProcessID
 run = do
   args <- getArgs
   let argsAsString = unwords args
-  (_, _, _, ph) <-
-    createProcess
-      (proc "bash" ["-c", "jetpack " ++ argsAsString]) {cwd = Nothing}
-  return ph
+  procId <- forkProcess Lib.run
+  return procId
