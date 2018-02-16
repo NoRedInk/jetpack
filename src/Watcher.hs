703c1f2a33d4f2dff3b6f6e96d26e3b88c2d6c58
Stoeffel <schtoeffel@gmail.com>
2018-02-16 17:13:58 +0100

    wip use rust notify

--- src/Watcher.hs
+++ src/Watcher.hs
@@ -9,6 +9,7 @@ import Data.Foldable (traverse_)
 import qualified Data.Text as T
 import Error
 import GHC.IO.Handle
+import qualified Notify
 import qualified System.Directory as Dir
 import System.Environment
 import System.Exit
@@ -23,51 +24,26 @@ import Twitch
         defaultMainWithOptions)
 
 watch :: Config.Config -> Args -> IO ()
-watch config args = do
-  mVar <- newMVar Nothing
+watch config args
+  -- mVar <- newMVar Nothing
+ = do
   putStrLn "Watching. Hit any key to exit."
-  rebuild config args mVar
-  processID <-
-    forkProcess $
-      -- redirect the process stdout and stderr to /dev/null. This probably won't
-      -- work on Windows, but then nothing here does yet so... ok?
-     do
-      defaultMainWithOptions (options config) $
-        traverse_
-          (addModify (const $ rebuild config args mVar))
-          fileTypesToWatch
-  -- TODO: we're swallowing any errors that occur here. We were before too, but
-  -- now we *explicitly* are.
-  getProcessStatus True False processID
+  -- rebuild config args mVar
+  -- processID <-
+  --   forkProcess $
+  --     -- redirect the process stdout and stderr to /dev/null. This probably won't
+  --     -- work on Windows, but then nothing here does yet so... ok?
+  --    do
+  --     defaultMainWithOptions (options config) $
+  --       traverse_
+  --         (addModify (const $ rebuild config args mVar))
+  --         fileTypesToWatch
+  -- -- TODO: we're swallowing any errors that occur here. We were before too, but
+  -- -- now we *explicitly* are.
+  -- getProcessStatus True False processID
+  Notify.watch "/Users/stoeffel/nri/noredink" (\_ -> Builder.build config args)
   return ()
 
 fileTypesToWatch :: [Dep]
 fileTypesToWatch =
   ["**/*.elm", "**/*.coffee", "**/*.js", "**/*.sass", "**/*.scss", "**/*.json"]
-
-options :: Config.Config -> Options
-options Config {source_directory} =
-  Options
-    NoLogger -- log
-    Nothing -- logFile
-    (Just source_directory) -- root
-    True -- recurseThroughDirectories
-    Twitch.Debounce
-    0.5 -- debounce time, in seconds.
-    0 -- pollInterval
-    False -- usePolling
-
-rebuild :: Config.Config -> Args -> MVar (Maybe ProcessID) -> IO ()
-rebuild config args mVar = do
-  killCurrentProcess mVar
-  processID <- forkProcess $ Builder.build config args
-  putMVar mVar (Just processID)
-
-killCurrentProcess :: MVar (Maybe ProcessID) -> IO ()
-killCurrentProcess mVar
-  -- takeMVar blocks if there is nothing inside it.
-  -- This prevents a race condition that could happen if multiple files are
-  -- written at once.
- = do
-  runningProcess <- takeMVar mVar
-  traverse_ (signalProcess softwareTermination) runningProcess
