a602651afbc6e8406c65de1e43b7af33f3018e7e
Stoeffel <schtoeffel@gmail.com>
2018-02-25 18:53:34 +0100

    adds commands

--- notifier/Notify.hs
+++ notifier/Notify.hs
@@ -1,10 +1,7 @@
 {-|
 Module      : Notify
-Description : Notify's main module
+Description : Filesystem notifications. This is a wrapper around rust's notify crate.
 
-This is a haddock comment describing your library
-For more information on how to write Haddock comments check the user guide:
-<https://www.haskell.org/haddock/doc/html/index.html>
 -}
 module Notify
   ( Config(..)
@@ -44,14 +41,55 @@ data Config = Config
   }
 
 watch :: Config -> IO ()
-watch Config { pathToWatch
-             , debounceInSecs
-             , relevantExtensions
-             , runAtStartup
-             , onChange
-             , onError
-             } = do
+watch config = do
   mVar <- newMVar Nothing
+  _ <- forkIO (start mVar config)
+  listenToCommands mVar config
+
+listenToCommands :: MVar (Maybe ProcessID) -> Config -> IO ()
+listenToCommands mVar (config@Config {onChange}) = do
+  value <- getChar
+  case commandFromStr value of
+    Just Rebuild -> do
+      _ <- startProcess mVar onChange
+      listenToCommands mVar config
+    Just Quit -> do
+      stopProcess mVar
+      putStrLn "Thanks for compiling with jetpack today. Have a great day!"
+    Just Help -> do
+      _ <- putStrLn "Help"
+      _ <- putStrLn "===="
+      _ <- putStrLn ""
+      _ <- putStrLn "r: rebuild"
+      _ <- putStrLn "h: help"
+      _ <- putStrLn "?: help"
+      listenToCommands mVar config
+    Just (Unknown str) -> do
+      putStrLn ("Unknown" ++ (str : ""))
+      listenToCommands mVar config
+    Nothing -> listenToCommands mVar config
+
+data Command
+  = Rebuild
+  | Quit
+  | Help
+  | Unknown Char
+
+commandFromStr :: Char -> Maybe Command
+commandFromStr 'r' = Just Rebuild
+commandFromStr 'q' = Just Quit
+commandFromStr 'h' = Just Help
+commandFromStr '\n' = Nothing
+commandFromStr char = Just (Unknown char)
+
+start :: MVar (Maybe ProcessID) -> Config -> IO ()
+start mVar Config { pathToWatch
+                  , debounceInSecs
+                  , relevantExtensions
+                  , runAtStartup
+                  , onChange
+                  , onError
+                  } = do
   when runAtStartup $ startProcess mVar onChange
   onChangeCb <- mkCallback $ callbackInProcess mVar onChange relevantExtensions
   onErrorCb <- mkCallback $ onErrorCallback onError
@@ -75,11 +113,15 @@ callbackInProcess mVar cb relevantExtensions pathC = do
 
 startProcess :: MVar (Maybe ProcessID) -> IO () -> IO ()
 startProcess mVar cb = do
+  stopProcess mVar
+  processId <- forkProcess cb
+  putMVar mVar (Just processId)
+
+stopProcess :: MVar (Maybe ProcessID) -> IO ()
+stopProcess mVar = do
   runningProcess <- takeMVar mVar
   traverse_ (signalProcess softwareTermination) runningProcess
   traverse_ (getProcessStatus True False) runningProcess -- here be dragons, potentially
-  processId <- forkProcess cb
-  putMVar mVar (Just processId)
 
 isRelevant :: FilePath -> [T.Text] -> Bool
 isRelevant path = elem (T.pack (takeExtension path))
