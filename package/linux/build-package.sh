178d4615ccd12294e9b3d10dc3203931f5fe548b
Stoeffel <schtoeffel@gmail.com>
2018-03-01 11:01:26 -0800

    mkdir

--- package/linux/build-package.sh
+++ package/linux/build-package.sh
@@ -22,4 +22,5 @@ BUILD="jetpack-${PLATFORM}"
 mkdir -p dist/package-scripts
 JETPACK="$(stack path --local-install-root)/bin/jetpack"
 cp "$JETPACK" dist/package-scripts/jetpack
+mkdir -p binaries
 tar zcvf "binaries/$BUILD".tgz -C dist/package-scripts/jetpack
--- package/mac/build-package.sh
+++ package/mac/build-package.sh
@@ -23,4 +23,5 @@ mkdir -p dist/package-scripts
 JETPACK="$(stack path --local-install-root)/bin/jetpack"
 cp "$JETPACK" dist/package-scripts/jetpack
 strip dist/package-scripts/jetpack
+mkdir -p binaries
 tar zcvf "binaries/$BUILD".tgz -C dist/package-scripts/jetpack
