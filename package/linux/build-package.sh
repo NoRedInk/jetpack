524c64a2cb57d13719b0a0c95683efd29b39e8d9
Stoeffel <schtoeffel@gmail.com>
2018-03-01 10:50:56 -0800

    fix tar

--- package/linux/build-package.sh
+++ package/linux/build-package.sh
@@ -22,4 +22,4 @@ BUILD="jetpack-${PLATFORM}"
 mkdir -p dist/package-scripts
 JETPACK="$(stack path --local-install-root)/bin/jetpack"
 cp "$JETPACK" dist/package-scripts/jetpack
-tar zcvf "binaries/$BUILD".tgz -C dist/package-scripts jetpack
+tar zcvf "binaries/$BUILD".tgz -C dist/package-scripts/jetpack
