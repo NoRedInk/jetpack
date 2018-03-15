a4742e2cff36ea8d6f2869d1c3e39d0a76e3d0e9
Stoeffel <schtoeffel@gmail.com>
2018-03-15 10:19:25 +0100

    remove unused build scripts

--- package/linux/build-in-docker.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-# copied from https://github.com/avh4/elm-format. Thanks <3
-
-set -ex
-
-docker build -t jetpack-dev-linux .
-docker run -v "$(pwd)":/jetpack -w /jetpack jetpack-dev-linux ./package/linux/build-package.sh
