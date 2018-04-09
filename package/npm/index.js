f10315a3f06328c2298a65fb150656911e34e9f6
Michael Glass <me@mike.is>
2018-04-09 15:29:05 +0200

    add prepublish step for binwrapping

--- package/npm/index.js
+++ package/npm/index.js
@@ -5,6 +5,7 @@ var binVersion = require(path.join(__dirname, "package.json"));
 
 var root = "https://github.com/NoRedInk/jetpack/releases/download/" + binVersion.version + "/jetpack";
 
+          'https://github.com/NoRedInk/jetpack/releases/download/2.0.15/jetpack-linux.tar.gz'
 module.exports = binwrap({
   binaries: ["jetpack"],
   urls: {
