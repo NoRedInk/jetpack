1663be17954e64c899134ad677afc0e74de081af
Stoeffel <schtoeffel@gmail.com>
2018-10-16 08:23:36 +0200

    update to latest version of binwrap

--- package/npm/index.js
+++ package/npm/index.js
@@ -1,11 +1,14 @@
 var binwrap = require("binwrap");
 var path = require("path");
 
-var binVersion = require(path.join(__dirname, "package.json"));
-
-var root = "https://github.com/NoRedInk/jetpack/releases/download/" + binVersion.version + "/jetpack";
+var packageInfo = require(path.join(__dirname, "package.json"));
+var root =
+  "https://github.com/NoRedInk/jetpack/releases/download/" +
+  packageInfo.version +
+  "/jetpack";
 
 module.exports = binwrap({
+  dirname: __dirname,
   binaries: ["jetpack"],
   urls: {
     "darwin-x64": root + "-osx.tar.gz",
